4d62ca4cb647e514fa3018f0320e4e0bc071acd5
nadiahum <37163666+nadiahum@users.noreply.github.com>
2018-04-15 17:11:56 +0800

    Update read.php

--- read.php
+++ read.php
@@ -4,16 +4,19 @@ $file = "backupdata.txt";
 $result = file_get_contents($file);
 
 $baris = explode("[R]", $result);
+echo "<p style='margin-left:400px'>";
+echo "--DATA CALON STAF ORGANISASI 2018--";
 
+echo "<div style='margin-left:250px'>";
 echo "<table border=7 width=50 height=60 cellpadding=5 cellspacing=5>";
 echo "<tr>";
 echo "  <th>NAME</th>";
 echo "  <th>AGE</th>";
 echo "  <th>EMAIL</th>";
-echo "	<th>NUMBER</th>";
+echo " <th>NUMBER</th>";
 echo "  <th>ADDRESS</th>";
-echo "	<th>DELETE</th>";
-echo "	<th>EDIT</th>";
+echo " <th>DELETE</th>";
+echo " <th>EDIT</th>";
 echo "</tr>";
 for($i =0; $i<count($baris)-1; $i++) {
     //echo $b . "<br>";
@@ -24,13 +27,15 @@ for($i =0; $i<count($baris)-1; $i++) {
     echo "  <td>" . $col[0] . "</td>";
     echo "  <td>" . $col[1] . "</td>";
     echo "  <td>" . $col[2] . "</td>";
-    echo "	<td>" . $col[3] . "</td>";
+    echo " <td>" . $col[3] . "</td>";
     echo "  <td>" . $col[4] . "</td>";
     echo '  <td> <a href="delete.php?row='.$i.'">DELETE</a> </td>';
     echo '  <td> <a href="edit.php?row='.$i.'">EDIT</a> </td>';
     echo "</tr>";
 }
 echo "</table>";
+echo "</div>";
+echo "</p>";
 
 ?>
 
